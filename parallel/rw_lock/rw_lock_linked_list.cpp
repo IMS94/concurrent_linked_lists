7114f8103fe0854bb82b66d482bd71208d0a293a
Imesha Sudasingha <imesha.sudasinghe@gmail.com>
2017-04-02 22:35:08 +0530

    Implemented serial linked list.

---
+++ parallel/rw_lock/rw_lock_linked_list.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+/**
+ * Struct to represent a list node
+ */
+struct list_node {
+    long value;
+    struct list_node *next;
+};
+
+/**
+ * Linked list class
+ */
+class rw_lock_linked_list {
+
+public:
+    struct list_node *head;
+
+    rw_lock_linked_list() {
+        head = NULL;
+    }
+
+    ~rw_lock_linked_list() {
+        head = NULL;
+    }
+
+    void insert(long value);
+
+    int member(long value);
+
+    int remove(long value);
+};
+
+/**
+ * Insert function will add the given element to the head of the list.
+ * @param value the value to be added to the list
+ */
+void rw_lock_linked_list::insert(long value) {
+    struct list_node *new_element = new list_node;
+    new_element->value = value;
+    new_element->next = head;
+
+    head = new_element;
+}
+
+/**
+ * Check for membership of a value inside the list
+ * @param value the value to be searched for membership
+ * @return 1 if a member | 0 otherwise
+ */
+int rw_lock_linked_list::member(long value) {
+    struct list_node *current_node = head;
+    while (current_node != NULL) {
+        if (current_node->value == value) {
+            return 1;
+        }
+        current_node = current_node->next;
+    }
+    return 0;
+}
+
+/**
+ * Removes the given value from the linked list
+ * @param value the value to be removed
+ * @return 1 if found and deleted | 0 otherwise
+ */
+int rw_lock_linked_list::remove(long value) {
+    struct list_node *current_node = head;
+    struct list_node *previous_node = NULL;
+
+    while (current_node != NULL) {
+        if (current_node->value == value) {
+            if (previous_node == NULL) {
+                head = NULL;
+            } else {
+                previous_node->next = current_node->next;
+            }
+            return 1;
+        }
+
+        previous_node = current_node;
+        current_node = current_node->next;
+    }
+
+    return 0;
+}
